f0507b62115e0daae64c86591b095890eebcccda
Timothy Tan <60724344+Timitan@users.noreply.github.com>
2025-04-03 15:49:26 -0700

    add script to filter questions with only declarative tag

---
+++ data_cleaning/swda_question_filter.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import difflib
+import glob
+import os
+import math
+import shutil
+
+start_string_index = -7
+end_string_index = -3
+file_name = "swda_declarative_final.csv"
+audio_folder = "./data_cleaning/swda_audio/questions/"
+filter_subfolder = "questions_filtered/"
+filtered_utt_df = pd.read_csv("./data_cleaning/" + file_name)
+
+def filter_only_question(row):
+    metatags = row["meta.tag"]
+    if metatags[start_string_index:end_string_index] == "qy^d":
+        audio_fname = str(row["audio_path_id"])
+        if os.path.isfile(audio_folder + audio_fname):
+            shutil.move(audio_folder + audio_fname, audio_folder + filter_subfolder + audio_fname)
+
+        return True
+    else:
+        return False
+    
+filtered_utt_df = filtered_utt_df[filtered_utt_df.apply(filter_only_question, axis=1)==True]
+print(filtered_utt_df)
+filtered_utt_df.to_csv("./data_cleaning/swda_declarative_final_filtered.csv", index=False)
