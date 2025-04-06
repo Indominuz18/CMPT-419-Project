b05daec8abca1cebc4c491173f5fc11884cc83fa
Timothy Tan <60724344+Timitan@users.noreply.github.com>
2025-04-05 18:46:16 -0700

    add file for testing statement vs question models

---
+++ data_cleaning/classifier_test.py
@@ -0,0 +1,51 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import pandas as pd
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+file_name = "swda_declarative_final.csv"
+question_utt_df = pd.read_csv("./data_cleaning/" + file_name)
+
+def predict(row):
+    text = row["clean_text"] #.replace("?", "")
+    inputs = tokenizer(text, return_tensors="pt")
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+        logits = outputs.logits
+
+    probs = F.softmax(logits, dim=1)
+
+    #print(text, probs)
+
+    predicted_class = torch.argmax(probs, dim=1).item()
+
+    prob_array = probs.numpy()[0]
+    row["class"] = predicted_class
+    row["prob_statement"] = prob_array[0]
+    row["prob_question"] = prob_array[1]
+
+    # print(predicted_class)
+    return row
+
+tokenizer = AutoTokenizer.from_pretrained("shahrukhx01/question-vs-statement-classifier")
+model = AutoModelForSequenceClassification.from_pretrained("shahrukhx01/question-vs-statement-classifier")
+
+question_utt_df2 = question_utt_df.apply(predict, axis=1)
+stats = question_utt_df2.groupby(['class']).size()
+print("shahrukhx01/question-vs-statement-classifier accuracy: " + str(stats[1] / (stats[0] + stats[1])))
+
+
+tokenizer = AutoTokenizer.from_pretrained("mrsinghania/asr-question-detection")
+model = AutoModelForSequenceClassification.from_pretrained("mrsinghania/asr-question-detection")
+
+question_utt_df2 = question_utt_df.apply(predict, axis=1)
+stats = question_utt_df2.groupby(['class']).size()
+print("mrsinghania/asr-question-detection accuracy: " + str(stats[1] / (stats[0] + stats[1])))
+
+# plt.scatter(question_utt_df["prob_statement"], question_utt_df["prob_question"])
+# plt.title("Scatterplot of Statement Probability vs Question Probability for Sentences in the Switchboard Dataset")
+# plt.xlabel("Statement Probability (%)")
+# plt.ylabel("Question Probability (%)")
+# plt.show()
